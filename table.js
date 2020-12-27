dfafaecffdbe63a50289a5589b909f4352863265
damatoaj <mr.arthurdamato@gmail.com>
2020-12-27 12:17:25 -0800

    got the switch players button to work

--- table.js
+++ table.js
@@ -26,6 +26,16 @@ function Crawler(x, y, width, height, color) {
 //king
 let king = new Crawler(200, 200, 50, 50, '#6b6f78')
 //defenders
+const defenders = [
+    new Crawler(150, 200, 50, 50, '#e4e7ed'),
+    new Crawler(100, 200, 50, 50, '#e4e7ed'),
+    new Crawler(250, 200, 50, 50, '#e4e7ed'),
+    new Crawler(300, 200, 50, 50, '#e4e7ed'),
+    new Crawler(200, 150, 50, 50, '#e4e7ed'),
+    new Crawler(200, 100, 50, 50, '#e4e7ed'),
+    new Crawler(200, 250, 50, 50, '#e4e7ed'),
+    new Crawler(200, 300, 50, 50, '#e4e7ed'),
+]
 let defender1 = new Crawler(150, 200, 50, 50, '#e4e7ed')
 let defender2 = new Crawler(100, 200, 50, 50, '#e4e7ed')
 let defender3 = new Crawler(250, 200, 50, 50, '#e4e7ed')
@@ -75,27 +85,48 @@ let checkForVictory = () => {
     detectCornerKing()
 }
 
-let defenderIndex = 0
-let selectDefender = defenderArray[defenderIndex]
+//set player switch button
+// let turnButton = document.getElementById("turns").addEventListener('onclick', selectPlayer)
 
-let switchDefender = () => {
-    defenderIndex++
-    if (defenderIndex == defenderArray.length) {
-        defenderIndex = 0
-    }
-    selectDefender = defenderArray[defenderIndex]
+
+let playerIndex = 0;
+let playerArray = [defenderArray, attackerArray]
+let selectPlayer = playerArray[playerIndex]
+let currentPiece = 0;
+
+let switchPlayer = (e) => {
+    playerIndex = (playerIndex+1) % 2
+    selectPlayer = playerArray[playerIndex] //selectplayer is the array of the current player's pieces
+    currentPiece = 0;
+   
+    console.log("player", playerIndex)
 }
+let turnButton = document.getElementById("turns").addEventListener('click', switchPlayer)
+console.log(playerIndex)
 
-let attackerIndex = 0
-let selectAttacker = attackerArray[attackerIndex]
+// //switch between defender characters
+// let defenderIndex = 0
+// let selectDefender = defenderArray[defenderIndex]
 
-let switchAttacker = () => {
-    attackerIndex++
-    if (attackerIndex == attackerArray.length) {
-        attackerIndex = 0
-    }
-    selectAttacker = attackerArray[attackerIndex]
-}
+// let switchDefender = () => {
+//     defenderIndex++
+//     if (defenderIndex == defenderArray.length) {
+//         defenderIndex = 0
+//     }    
+//     selectDefender = defenderArray[defenderIndex]
+// }    
+
+// //switch between attacking characters
+// let attackerIndex = 0
+// let selectAttacker = attackerArray[attackerIndex]
+
+// let switchAttacker = () => {
+//     attackerIndex++
+//     if (attackerIndex == attackerArray.length) {
+//         attackerIndex = 0
+//     }    
+//     selectAttacker = attackerArray[attackerIndex]
+// }    
 
 
 //move my hero based on the key pressed.
@@ -104,54 +135,56 @@ let movementDefender = (e) => {
     if (e.key === 'w') { //move up
         if (selectDefender.y - movement >= min_height) {
             selectDefender.y -= movement
-        }
+        }    
 
     } else if (e.key === 'a') { //move left
         if (selectDefender.x - movement >= min_height) {
             selectDefender.x -= movement
-        }
-    } else if (e.key === 's') {
+        }    
+    } else if (e.key === 's') { //move down
         if(selectDefender.y + movement <=max_height) {
-        selectDefender.y += movement
+        selectDefender.y += movement    
         }
-    } else if (e.key === 'd') {
+    } else if (e.key === 'd') { //move right
         if(selectDefender.x + movement <= max_width) {
-        selectDefender.x += movement
+        selectDefender.x += movement    
         }
     } else if (e.which == '32') {
         switchDefender()
-    }
+    }    
     console.log(`x: ${selectDefender.x}, y: ${selectDefender.y}`)// 
     checkForVictory()
-}
-let movementAttacker = (e) => {
+}    
+let movementAttacker = (e) => { //same movement codes but for attacking pieces
     if (e.key === 'i') { //move up
         if (selectAttacker.y - movement >= min_height) {
             selectAttacker.y -= movement
-        }
+        }    
 
     } else if (e.key === 'j') { //move left
         if (selectAttacker.x - movement >= min_height) {
             selectAttacker.x -= movement
-        }
-    } else if (e.key === 'k') {
+        }    
+    } else if (e.key === 'k') { //  move down
         if(selectAttacker.y + movement <=max_height) {
-        selectAttacker.y += movement
+        selectAttacker.y += movement    
         }
-    } else if (e.key === 'l') {
+    } else if (e.key === 'l') { //move right
         if(selectAttacker.x + movement <= max_width) {
-        selectAttacker.x += movement
+        selectAttacker.x += movement    
         }
     } else if (e.which == '76') {
         switchAttacker()
-    }
+    }    
     console.log(`x: ${selectAttacker.x}, y: ${selectAttacker.y}`)// 
     checkForVictory()
-}
+}    
 
 //set listener event for key down
 document.addEventListener('keydown', movementDefender)
 document.addEventListener('keydown', movementAttacker)
+
+
 
 // //helper function so my computer doesn't explode
 // document.querySelector('#btm-left').addEventListener('click', () => {
